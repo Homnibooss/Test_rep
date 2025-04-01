3a4b456f323fba5112829b19b67f3db56add4afe
Egor <homnibooss3@gmail.com>
2025-04-01 23:32:37 +0300

    First_commit

---
+++ Assets/Scripts/CloudController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NZ
+{
+
+    public class CloudController : MonoBehaviour
+    {
+
+        public void Awake()
+
+        {
+            Debug.Log("Cloud", this);
+        }
+
+    }
+}
